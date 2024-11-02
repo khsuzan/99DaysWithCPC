7b34186aa7b959782a2d83843099d3e62ad1a418
Kawsar Hossain <khsbd2@gmail.com>
2024-11-02 23:01:10 +0600

    initially created assigment screens

---
+++ 01_assignment_tracker/lib/screens/assignments/add_edit_assignment/screen_add_edit_assignment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenAddEditAssignment extends StatefulWidget {
+  const ScreenAddEditAssignment({super.key});
+
+  @override
+  State<ScreenAddEditAssignment> createState() => _ScreenAddEditAssignment();
+}
+
+class _ScreenAddEditAssignment extends State<ScreenAddEditAssignment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ 01_assignment_tracker/lib/screens/assignments/list_assignments/screen_list_assignments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenListAssignments extends StatefulWidget {
+  const ScreenListAssignments({super.key});
+
+  @override
+  State<ScreenListAssignments> createState() => _ScreenListAssignmentsState();
+}
+
+class _ScreenListAssignmentsState extends State<ScreenListAssignments> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
